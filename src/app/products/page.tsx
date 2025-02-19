d082eec463088e8798eb227eabb6d4099e52f310
luisplacco <lfelipe0507@gmail.com>
2025-02-18 23:28:04 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+const ProductPage = () => {
+    return ( <div className="p-5 border bg-slate-800 border-red-600 rounded-xl">Product Page
+    
+        <h1 className="text-red-800"> Product Page</h1>
+        <button> clica ai </button>
+    
+    </div> );
+}
+ 
+export default ProductPage;
